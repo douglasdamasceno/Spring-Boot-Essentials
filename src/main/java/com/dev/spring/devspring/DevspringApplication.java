3dd3ef36f14b5a6787394ee9d0842abd14a22d1f
Douglas Damasceno <douglasmdamasceno@gmail.com>
2022-04-21 12:04:31 -0300

    add configuração do crossorigin centralizada

--- src/main/java/com/dev/spring/devspring/DevspringApplication.java
+++ src/main/java/com/dev/spring/devspring/DevspringApplication.java
@@ -3,16 +3,26 @@ package com.dev.spring.devspring;
 import org.springframework.boot.SpringApplication;
 // import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // import org.springframework.context.annotation.ComponentScan;
 
 // @EnableAutoConfiguration
 // @ComponentScan
 // (basePackages = "com.dev.spring.devspring")
 @SpringBootApplication
-public class DevspringApplication {
+public class DevspringApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevspringApplication.class, args);
 	}
-
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/animes/**")
+			.allowedMethods("POST","GET","PUT","DELETE")
+			.allowedOrigins("http://localhost:3000");
+		WebMvcConfigurer.super.addCorsMappings(registry);
+	}
 }
